9df561156c5ff9981c74bafa9c90a2b5e467a8ee
Haris <muhamad_haris@hotmail.com>
2017-10-19 14:20:59 +0800

    Add profile registration page

---
+++ app/controllers/foodies_controller.rb
@@ -0,0 +1,38 @@
+class FoodiesController < ApplicationController
+
+	before_action :authenticate_foody!, only: [:edit, :update]
+	before_action :check_authorisation, only: [:edit, :update]
+	before_action :set_foodies
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @foody.update(foody_params)
+			flash.now[:alert] = 'Succesfully saved'
+			redirect_to @foody
+		else
+			flash.now[:alert] = 'Do fill any blanks'
+			render :edit
+	end
+
+	private
+
+	def check_authorisation
+		unless current_foody.id == params[:id].to_i
+			redirect_to root_url
+		end
+	end
+
+	def set_foodies
+		@foody = Foody.find(params[:id])
+	end
+
+	def foody_params
+		require(:foody).permit(:name, :status)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :foodies
+  devise_for :foodies, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: "home#index"
+
+	resources :foodies, only: [:show, :edit, :update]
 
 end
