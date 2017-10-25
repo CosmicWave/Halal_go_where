9c9e780d937dd7d569a245b0908381baf3d05308
Haris <muhamad_haris@hotmail.com>
2017-10-25 17:58:19 +0800

    Routes worked

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  devise_for :foodies, :controllers => { :omniauth_callbacks => "foodies/omniauth_callbacks" }
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	devise_for :foodies, controllers: { registrations: "registrations", omniauth_callbacks: "foodies/omniauth_callbacks"  }
