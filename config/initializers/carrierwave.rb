3684fd94bf6775746b484ba66792b0a2793f9a6f
Regent Chien <regent.chien@hotmail.sg>
2017-11-14 11:56:18 +0800

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    aws_access_key_id:     'AKIAIF7PH3XHNPZDHNOA',        # required
+    aws_secret_access_key: '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'        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = 'halalgowhere3'             # required
 end
